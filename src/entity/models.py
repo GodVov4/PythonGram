738cb434786168a275eb93639813e7f4fc94eb4e
DSofiya <134394822+DSofiya@users.noreply.github.com>
2024-01-11 19:55:28 +0200

    Update models.py

---
+++ src/entity/models.py
@@ -0,0 +1,45 @@
+import enum
+from datetime import date
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String,Integer,ForeignKey, DateTime, func, Enum,Boolean
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
+    role: Mapped[Enum] = mapped_column("role", Enum(Role), default=Role.user, nullable=True)
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
+    ban:  Mapped[bool] = mapped_column(default=False, nullable=True)
+    number_foto: Mapped[int] = mapped_column(Integer, nullable=True)
+    blacklisted_tokens: Mapped["Blacklisted"] = relationship("Blacklisted", backref="users", lazy="joined")
+    
+
+class Blacklisted(Base):
+    __tablename__ = "blacklisted"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=True)
+    token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    user = relationship("User", back_populates="blacklisted_tokens")
+
