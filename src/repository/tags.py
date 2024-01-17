1b63c51e0ea6fc97d4bc1f47f38dbba982db482a
fls0 <134416720+fls0@users.noreply.github.com>
2024-01-17 15:44:07 +0100

    pep9

--- src/repository/tags.py
+++ src/repository/tags.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.entity.models import Tag
 
 
-
 async def create_tag(tag: Tag, db: AsyncSession):
     stmt = select(Tag).filter_by(name=tag.name)
     db_tag = await db.execute(stmt)
