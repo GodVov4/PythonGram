9ed4fb59afccd3b273cf61a7ebdf325574e9dbc7
GennadiiBukh <gennadii.bukh@gmail.com>
2024-01-25 08:55:00 +0200

    Update transoform, add dockstr

--- src/schemas/transform.py
+++ src/schemas/transform.py
@@ -5,19 +5,44 @@ from pydantic import BaseModel, Field
 
 
 class TransformCreate(BaseModel):
+    """
+    The TransformCreate model is used to create a new transformed picture.
+
+    :param transformation_params: The transformation parameters to be applied to the image.
+    :return: The transformed picture.
+    """
     transformation_params: Dict[str, Union[str, int]] = Field(
         default_factory=dict,
-        example={"width": 500, "height": 300, "crop": "fill",
-                 "effect": "grayscale"
+        example={"width": 500, "height": 300,
+                 "crop": "fill",
+                 "effect": "grayscale",
+                 "border": "5px_solid_lightblue",
+                 "angle": 15,
                  }
     )
 
 
 class TransformUpdate(TransformCreate):
+    """
+    The TransformUpdate model is used to update an existing transformed picture.
+
+    :param transformation_params: The transformation parameters to be applied to the image.
+    :return: The updated transformed picture.
+    """
     pass
 
 
 class TransformResponse(BaseModel):
+    """
+    The TransformResponse model is used to return a transformed picture.
+
+    :param id: The ID of the transformed picture.
+    :param original_picture_id: The ID of the original picture.
+    :param url: The URL of the transformed picture.
+    :param qr_url: The URL of the QR code.
+    :param user_id: The ID of the user who created the transformed picture.
+    :return: The transformed picture.
+    """
     id: int
     original_picture_id: int
     url: str
